507da4277b2f8aab345a9afbeef5d25d9683027e
unknown <you@example.com>
2020-12-18 16:38:34 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+var express = require("express");
+
+var server = express();
+const PORT = process.env.PORT || 5000
+server.get("/", (req, res, next) => {
+    console.log("some one get menu");
+    res.send("menu: what do you want? food or water");
+})
+
+server.listen(PORT, () => {
+    console.log("server is running on port 3000");
+})
